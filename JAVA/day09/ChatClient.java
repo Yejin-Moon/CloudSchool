dcacc88cc6c0d3717fb8b68101c848f9b37f76a8
Yejin-Moon <dpwls991226@naver.com>
2023-06-23 15:27:42 +0900

    Server와 Client가 1대1로 채팅 주고받기

---
+++ JAVA/day09/ChatClient.java
@@ -0,0 +1,50 @@
+package day09;
+
+import java.io.*; import java.net.*;
+
+//키보드로 입력받은 데이터를 채팅 상대에게 전송하는 스래드
+class DataSender extends Thread {
+    private OutputStream out ;
+    public DataSender(OutputStream out ){ this.out = out ; }
+    public void run() {
+        byte[] buf = new byte[1024];
+        int count = 0 ;
+        try{
+            while( (count = System.in.read(buf))!= -1 ) //키보드로 입력
+            { out.write(buf,0,count); //채팅 상대에게 전송
+                }
+            out.close();
+        }catch(IOException e){e.printStackTrace(); }
+    }
+}
+
+// 채팅상대에게 전송받은 데이터를 Console에 출력하는 스래드
+class DataReciever extends Thread {
+    InputStream in;
+    public DataReciever(InputStream in) { this.in = in; }
+    public void run() {
+        byte[] data = new byte[1024];
+        int count = 0;
+        try { //채팅 상대에게 데이터받음
+            while ((count = in.read(data)) != -1)
+            { // Console에출력
+                System.out.write(data, 0, count);
+                System.out.flush();
+            }
+            in.close();
+        } catch (IOException e) { e.printStackTrace();
+        }
+    }
+}
+
+public class ChatClient {
+    public static void main(String[] args) throws Exception {
+        InetAddress anotherUser = InetAddress.getByName("localhost") ;
+        Socket sock = new Socket(anotherUser,10000);
+        //또는 sock = new Socket("127.0.0.1", 10001);
+        OutputStream out = sock.getOutputStream();
+        InputStream in = sock.getInputStream();
+        new DataSender(out).start();
+        new DataReciever(in).start();
+}
+}
