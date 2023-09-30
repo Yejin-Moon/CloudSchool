210c7c2d38b5196916f01f4ffdef536ff25d781d
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2023-09-30 21:55:44 +0900

    view 환경설정
    기본 인덱스 페이지 생성

---
+++ JAVA/spring_basic/hello-spring/src/main/java/inflearn/hellospring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package inflearn.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {//spring 에서 모델 만들어서 데이터 넣어서 줌
+        model.addAttribute("data","hello!!");
+        return "hello"; //templates 밑에 있는 hello.html을 찾아서 타임리프 템플릿 엔진 처리
+        //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버가 화면을 찾아서 처리한다.
+        //resources/templates/hello.html을 찾아서 처리
+    }
+}
