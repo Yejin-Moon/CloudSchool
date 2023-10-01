4bdc97a018008fb0612dced02722becb463142ec
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2023-10-01 13:27:01 +0900

    mvc

--- JAVA/spring_basic/hello-spring/src/main/java/inflearn/hellospring/controller/HelloController.java
+++ JAVA/spring_basic/hello-spring/src/main/java/inflearn/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package inflearn.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,11 @@ public class HelloController {
         return "hello"; //templates 밑에 있는 hello.html을 찾아서 타임리프 템플릿 엔진 처리
         //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버가 화면을 찾아서 처리한다.
         //resources/templates/hello.html을 찾아서 처리
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
