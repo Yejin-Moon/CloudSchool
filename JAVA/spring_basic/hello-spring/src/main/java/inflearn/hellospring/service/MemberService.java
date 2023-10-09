10a0147e5bd1ac72375da9b44f26e886cee42cb7
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2023-10-09 22:31:36 +0900

    회원 서비스 테스트 작성

--- JAVA/spring_basic/hello-spring/src/main/java/inflearn/hellospring/service/MemberService.java
+++ JAVA/spring_basic/hello-spring/src/main/java/inflearn/hellospring/service/MemberService.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원 가입
     public Long join(Member member) {
@@ -28,7 +32,7 @@ public class MemberService {
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
-    
+
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
